08d10a0469208cdb1790f6f8958ce3845c86ff57
Pedro Henrique de Sousa Varela <78501819+Pedrohsv1@users.noreply.github.com>
2023-09-14 19:48:45 -0300

    aula 03

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import fastify from "fastify";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletion } from "./routes/generate-ai-completion";
+import fastifyCors from "@fastify/cors";
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletion);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("Server is running!");
+  });
